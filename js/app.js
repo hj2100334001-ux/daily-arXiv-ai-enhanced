0cd05ec099265a90a28252acc31cf611619d08af
hj2100334001-ux <v-huangjie@microsoft.com>
2026-07-08 07:00:08 -0700

    fix: avoid timezone shift when showing dates

--- js/app.js
+++ js/app.js
@@ -1668,7 +1668,8 @@ function toggleView() {
 }
 
 function formatDate(dateString) {
-  const date = new Date(dateString);
+  const [year, month, day] = dateString.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
   return date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'numeric',
